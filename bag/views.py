2c744fdff0306a3a363259dd1eeb3d71897587e0
Slava B <slbabin@gmail.com>
2023-11-29 21:50:18 +0000

    Update bag quantigy function

--- bag/views.py
+++ bag/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
-
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
-
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -35,10 +33,10 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -53,7 +51,6 @@ def adjust_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-
     else:
         if quantity > 0:
             bag[item_id] = quantity
@@ -65,7 +62,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Remove the item from the shopping bag """
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
@@ -74,14 +71,14 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
 
         if size:
-            del bag[item_id]['items_by_size'][size] 
+            del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-
         else:
             bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
+
     except Exception as e:
         return HttpResponse(status=500)
