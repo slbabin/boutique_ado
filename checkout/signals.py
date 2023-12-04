04278811ea64af0b8039a8fe8579a723822495fa
Slava B <slbabin@gmail.com>
2023-12-04 21:35:08 +0000

    Updated checkout

--- checkout/signals.py
+++ checkout/signals.py
@@ -15,6 +15,4 @@ def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-    print('Delete signal received!!')
-    
     instance.order.update_total()
